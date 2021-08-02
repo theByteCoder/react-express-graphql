0d1b6859b8658298d191876312866f386022f53f
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2021-08-02 22:38:43 +0530

    feat(app): Added material ui table to list component

--- react-graphql/src/components/Lists.js
+++ react-graphql/src/components/Lists.js
@@ -1,24 +1,61 @@
-import { useEffect } from 'react'
 import { useQuery } from 'react-query'
 
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
 const Lists = () => {
-    const { isLoading, error, data } = useQuery('@books', () => fetch('http://localhost:2000/graphql', {
+    const classes = useStyles();
+
+    const { isLoading, error, data: rows } = useQuery('@books', () => fetch('http://localhost:2000/graphql', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: '{ books { name genre } }' })
+        body: JSON.stringify({ query: '{ books { name genre id: _id } }' })
     }).then(res => res.json()).then(({ data: { books: results } }) => results)
     )
 
-    return <>
-        <ul>
-            {data.map((item, i) => {
-                return <li>
-                    <div>{item.name}</div>
-                    <div>{item.genre}</div>
-                </li>
-            })}
-        </ul>
-    </>
+    useEffect(() => {
+        console.log(rows);
+    }, [rows])
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Genre</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {(!error && !isLoading) ? rows?.map((row) => (
+                        <TableRow key={row.id}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.genre}</TableCell>
+                        </TableRow>
+                    )) : <TableRow >
+                        <TableCell component="th" scope="row">
+                            Loading data
+                        </TableCell>
+                    </TableRow>}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
 
 export default Lists
